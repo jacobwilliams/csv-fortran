54f99cf03028abb2bd5f32589f0e1fe1b44f5aa2
Jacob Williams <jacobwilliams@users.noreply.github.com>
2019-11-29 07:32:24 -0600

    added more cases to the write test.

--- src/tests/csv_write_test.f90
+++ src/tests/csv_write_test.f90
@@ -7,6 +7,9 @@ implicit none
 
 type(csv_file) :: f
 logical :: status_ok
+real(wp),dimension(3),parameter :: xvec = [1.0_wp, 2.0_wp, 3.0_wp]
+integer,dimension(4),parameter :: ivec = [1,2,3,4]
+logical,dimension(4),parameter :: lvec = [.true.,.true.,.true.,.true.]
 
 ! open the file
 call f%open('test.csv',n_cols=4,status_ok=status_ok)
@@ -21,6 +24,17 @@ call f%add(.true.)
 call f%next_row()
 call f%add([4.0_wp,5.0_wp,6.0_wp],real_fmt='(F5.3)')
 call f%add(.false.)
+call f%next_row()
+call f%add(xvec,real_fmt='(F5.3)')
+call f%add(.true.)
+call f%next_row()
+call f%add([1,2,3,4])
+call f%next_row()
+call f%add(ivec)
+call f%next_row()
+call f%add([.true.,.true.,.true.,.true.])
+call f%next_row()
+call f%add(lvec)
 call f%next_row()
 
 ! finished
