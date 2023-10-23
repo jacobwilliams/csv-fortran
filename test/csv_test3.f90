a0424b6fe6969f3a39db7a02b4d1c1541b7e2951
Jacob Williams <jacobwilliams@users.noreply.github.com>
2023-10-23 00:05:44 -0500

    more tests

--- test/csv_test3.f90
+++ test/csv_test3.f90
@@ -14,6 +14,8 @@
     character(len=30),dimension(:),allocatable :: header, col1
     integer :: i
     character(len=100) :: tmp_str
+    integer,dimension(:),allocatable :: int_col
+    logical,dimension(:),allocatable :: log_col
 
     write(*,*) ''
     write(*,*) '============================'
@@ -25,27 +27,37 @@
     call f%initialize(verbose = .true.)
 
     ! open the file
-    call f%open('test.csv',n_cols=4,status_ok=status_ok)
+    call f%open('test.csv',n_cols=6,status_ok=status_ok)
 
     ! add header
-    call f%add(['x','y','z','t'])
+    call f%add(['x  ','y  ','z  ','t  ','int','str'])
     call f%next_row()
 
     ! add some data:
     call f%add([1.0_wp,2.0_wp,3.0_wp],real_fmt='(F5.3)')
     call f%add(.true.)
+    call f%add(1)
+    call f%add('a')
     call f%next_row()
     call f%add([2.0_wp,5.0_wp,6.0_wp],real_fmt='(F5.3)')
     call f%add(.false.)
+    call f%add(1)
+    call f%add('b')
     call f%next_row()
     call f%add([3.0_wp,5.0_wp,6.0_wp],real_fmt='(F5.3)')
     call f%add(.false.)
+    call f%add(1)
+    call f%add('c')
     call f%next_row()
     call f%add([4.0_wp,5.0_wp,6.0_wp],real_fmt='(F5.3)')
     call f%add(.false.)
+    call f%add(1)
+    call f%add('d')
     call f%next_row()
     call f%add([5.0_wp,5.0_wp,6.0_wp],real_fmt='(F5.3)')
     call f%add(.false.)
+    call f%add(1)
+    call f%add('e')
     call f%next_row()
 
     ! finished
@@ -70,11 +82,16 @@
     do i = 1, size(col1)
       print "(*(g0))", ">",trim(col1(i)),"<"
     end do
-
     do i = 1, 5
       write(tmp_str,'(F5.3)') real(i,wp)
       if (col1(i) /= tmp_str) error stop 'error converting cell to string:'//tmp_str
     end do
+
+    call f%get(4,log_col,status_ok)
+    if (.not. all(log_col .eqv. [.true.,.false.,.false.,.false.,.false.])) error stop 'error getting logical column'
+
+    call f%get(5,int_col,status_ok)
+    if (.not. all(int_col==1)) error stop 'error getting integer column'
 
     ! destroy the file
     call f%destroy()
